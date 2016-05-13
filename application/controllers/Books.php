970e44f5f1151b53f682ede57d03a7003a4a6d7c
dbdario025 <dbdario025@gmail.com>
2016-05-12 22:30:42 -0500

    CreacionProyectoDePrueba
    Se realizan los primeros pasos para crear servicio RestFull

---
+++ application/controllers/Books.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Books extends REST_Controller {
+	
+	public function index_get()
+	{
+		echo 1;
+	}
+	public function find_get($id)
+	{
+		
+	}
+	public function index_post()
+	{
+		
+	}
+	public function index_put()
+	{
+		
+	}
+	public function index_delete()
+	{
+		
+	}
+}
